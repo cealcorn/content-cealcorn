b02cbfe999047d7c8bebf640111f4d10aed89d1a
Shawn Wells <shawn@shawnwells.io>
2026-03-20 13:26:28 -0400

    Revert "makes it build"

--- ssg/utils.py
+++ ssg/utils.py
@@ -21,9 +21,7 @@ class SSGError(RuntimeError):
     pass
 
 
-# Match product IDs that end with a numeric version while allowing digits in the
-# product prefix itself, such as bascontrol22d4 -> bascontrol22d + 4.
-PRODUCT_NAME_PARSER = re.compile(r"(.+?)([0-9]+)$")
+PRODUCT_NAME_PARSER = re.compile(r"([a-zA-Z\-]+)([0-9]+)")
 
 
 class VersionSpecifierSet(set):
